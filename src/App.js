4af96dcb84a72233d2dc032ef6aef0cc118d0995
Kjartan Kristjansson <kjarisk@gmail.com>
2019-01-16 08:34:45 +0100

    Test, polyfill, cdn, analyzer, webpack

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component, lazy, Suspense } from 'react';
+import { hot } from 'react-hot-loader';
+
+const Warning = lazy(() => import('./Warning'));
+
+class App extends React.Component {
+  state = {
+    count: 0,
+  }
+
+increment = () => {
+  this.setState(state => ({ count: state.count + 1}));
+}
+
+decrement = () => {
+  this.setState(state => ({ count: state.count - 1}));
+}
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Test</h1>
+        <h2 className={count > 10 ? 'warning' : null}>
+          Count: {count}
+        </h2>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+        {count > 10 ? 
+          <Suspense fallback={null}>
+            <Warning />
+          </Suspense>
+          : null}
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
